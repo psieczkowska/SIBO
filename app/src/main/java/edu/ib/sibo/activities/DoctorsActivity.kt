5375ec122f58bca400d772756c32c8b90636c9d6
Paulina Sieczkowska <paulina.sieczkowska@wp.pl>
2021-12-22 12:03:28 +0100

    Searching products

---
+++ app/src/main/java/edu/ib/sibo/activities/DoctorsActivity.kt
@@ -0,0 +1,11 @@
+package edu.ib.sibo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctors)
+    }
+}
